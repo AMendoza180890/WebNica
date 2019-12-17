ed605414bb2ab8323e990b989178c66c2dd210b8
AMendoza <allanmaleman@gmail.com>
2019-12-17 15:23:47 -0600

    1_modificacion_Logica_Conexion

--- Modelos/conexionBD.php
+++ Modelos/conexionBD.php
@@ -1,8 +1,28 @@
 <?php
 class ConexionBD{
+    private $host;
+    private $db;
+    private $user;
+    private $passw;
+    private $charset;
+
+    public function _construct(){
+        $this->host =   $_SERVER;
+        $this->db =   'dbwebnica';
+        $this->user =   'root';
+        $this->passw =   '';
+        /*$this->charset =   'localhost';*/
+    }
+
     static public function cBD(){
-        $bd = new PDO("mysql:host=localhost;dbname=dbwebnica","root","");
-        return $bd;
+        try{
+            $bd = new PDO("mysql:host=".$this->host.";dbname=".$this->db,$this->user,$this->passw);
+
+            return $bd;    
+        }
+        catch (PDOException $ex){
+            echo "Error en la conexion".$ex->getMessage();
+        }    
     }
 }
 ?>
