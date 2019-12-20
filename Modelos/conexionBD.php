dbcaf0c16be7e136e4193e4dca465b18242b1353
AMendoza180890 <allanmaleman@gmail.com>
2019-12-20 17:41:50 -0600

    se modifico conexionBD.php, el host es el de web-nica publico

--- Modelos/conexionBD.php
+++ Modelos/conexionBD.php
@@ -7,7 +7,7 @@ class ConexionBD{
 
     public function __construct()
     {
-        $this->host     =   $_SERVER['SERVER_NAME'];
+        $this->host     =   'mysql1007.mochahost.com';
         $this->db       =   "marvin_dbwebnica";
         $this->user     =   "marvin_userWebNi";
         $this->passw    =   "Soloyo246$$";
@@ -15,7 +15,7 @@ class ConexionBD{
             
     public  function cBD(){
         try{
-            $conexion = "mysql:host=". $this->host.";dbname=". $this->db;
+            $conexion = "mysql:host=".$this->host.";dbname=".$this->db;
 
             $bd = new PDO($conexion, $this->user, $this->passw);
             
