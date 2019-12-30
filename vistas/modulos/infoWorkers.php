8a4d103e69a2d3fee875d22eeeaebd3998fa07e4
AMendoza180890 <allanmaleman@gmail.com>
2019-12-29 18:44:51 -0600

    Se corrigio acentos de textos usando utf8_encode

--- vistas/modulos/infoWorkers.php
+++ vistas/modulos/infoWorkers.php
@@ -1,14 +1,6 @@
 <?php
 $worker = new EmpleadoC();
 $workerC = $worker::empleadoCont();
-/*
-$picture = ['Luci1.svg', 'Edelma2.svg', 'Allan1.svg'];
-$name = ['Lucia Silva', 'Edelma Reyes', 'Allan Mendoza'];
-$vocation = ['Lic. En Diseño Gráfico.', 'Lic. Comunicación Social', 'Ing. En Computación y Telemática.'];
-$Email = ['lucia@web-nica.com', 'edelma@web-nica.com', 'allan@web-nica.com'];
-$Description =['Pendiente', '<small> Dos años de experiencia en redacción web en el periódico El Nuevo Diario. Actualmente labora como Content Manager en Origami Brand Studio.</small>',
-    '<small> Cuatro años de responsable de Tecnología en ONG Tesoros de Dios.<br> Desarrollador web y de aplicaciones de escritorio.</small>'];
-    */
     for ($i=0; $i < count($workerC); $i++) {         
         echo '<div class="col-md-4">';
         echo '<div class="fa-container">';
@@ -16,16 +8,15 @@ $Description =['Pendiente', '<small> Dos años de experiencia en redacción web 
         echo '<i class="fa fa-comment-o fa-3x enable" aria-hidden="true"></i>';
         echo '</div>';
         echo '<div class="text-center">';
-        echo '<h3>' . $workerC[$i]->CatEmpNombres. '</h3>';
+        echo '<h3>' . utf8_encode($workerC[$i]->CatEmpNombres). '</h3>';
         echo '</div>';
         echo '<div>';
         echo '<p class="text-center">';
-        echo $workerC[$i]->CatEmpVocacion. '<br>';
-        echo $workerC[$i]->CatEmpEmail.'<br>';
-        echo $workerC[$i]->CatEmpDescription;
+        echo utf8_encode($workerC[$i]->CatEmpVocacion). '<br>';
+        echo utf8_encode($workerC[$i]->CatEmpEmail).'<br>';
+        echo utf8_decode($workerC[$i]->CatEmpDescription);
         echo '</p>';
         echo '</div>';
         echo '</div>';
     }
-
 ?>
