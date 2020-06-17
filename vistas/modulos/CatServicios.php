76e549e04284bed3e5c0c7849484568590719256
AMendoza180890 <allanmaleman@gmail.com>
2020-06-16 19:27:56 -0600

    mejorado el modulo de servicios de forma automatica

--- vistas/modulos/CatServicios.php
+++ vistas/modulos/CatServicios.php
@@ -1,32 +1,35 @@
-            <?php
-            try {
-            $ViewCatServicios = new CatServiciosViewControler();
-                $servicios = $ViewCatServicios::CatServicioViewCont();
+<?php
+    try {
+    $ViewCatServicios = new CatServiciosViewControler();
+    $servicios = $ViewCatServicios::CatServicioViewCont();
 
-                            for ($j=0; $j < count($servicios); $j++) {
-                                    if ($j == 0 || $j == 4) {
-                                        echo '<div class="row">';
-                                    }
-                                        echo '<div class="col-12 col-lg-3">
-                                            <div class="fa-container">
-                                                <i class="'.$servicios[$j]->CatServIcono.' fa-3x" aria-hidden="true"></i>
-                                            </div>
+        for ($i=0; $i < count($servicios); $i++) { 
+            echo '<div class="row">';
+                for ($j=0; $j < 4; $j++) {
+                    if (isset($servicios[$i])) {
+                        echo '<div class="col-xs-6 col-lg-3">
+                                <div class="fa-container">
+                                    <i class="'.$servicios[$i]->CatServIcono.' fa-3x" aria-hidden="true"></i>
+                                </div>
+                    
+                                <div class="text-center">
+                                    <h3>'.utf8_decode($servicios[$i]->CatServTitulo).'</h3>
+                                </div>
+                    
+                                <div>
+                                    <p class="noto text-center">'.utf8_decode($servicios[$i]->CatServDescripcion).'</p>
+                                </div>
+                            </div>';
+                        $i = $i + 1;
+                    } else {
+                        break;
+                    }   
+                }
+                    echo '</div>';
+                    $i = $i - 1;
+        }
 
-                                            <div class="text-center">
-                                                <h3>'.utf8_decode($servicios[$j]->CatServTitulo).'</h3>
-                                            </div>
-
-                                            <div>
-                                                <p class="noto text-center">'.utf8_decode($servicios[$j]->CatServDescripcion) .'</p>
-                                            </div>
-                                        </div>';
-                                if ($j == 3 || $j == 7) {
-                                    echo '</div>';
-                                }
-                            }
-            } catch (Exception $ex) {
-                mensajes::error($ex);
-            }
-                  
-
-            ?>
+    } catch (Exception $ex) {
+        mensajes::error($ex);
+}
+?>
--- vistas/modulos/about.php
+++ vistas/modulos/about.php
@@ -1,55 +1,57 @@
   <?php
-    $headerAbout = new serviciosC();
-    $headerAbout -> servicioHeaderC();
+  echo '<div class="comtainer"';
+      $headerAbout = new serviciosC();
+      $headerAbout->servicioHeaderC();
+  echo '</div>';
   ?>
 
-        <div class="section-container border-section-container">
-          <div class="container">
-            <?php
-              include 'vistas/modulos/CatServicios.php';
-            ?>
-          </div>
-        </div>
+  <div class="section-container border-section-container">
+    <div class="container">
+      <?php
+      include 'vistas/modulos/CatServicios.php';
+      ?>
+    </div>
+  </div>
 
-        <div class="section-container">
-          <div class="container">
-            <div class="row">
-              <div class="col-xs-12">
-                <div class="text-justify">
+  <div class="section-container">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="text-justify">
 
-                  <h2 class="text-center">Nuestro proceso de creación </h2>
+            <h2 class="text-center">Nuestro proceso de creación </h2>
 
-                  <ol class="features">
-                    <li>
-                      <small class="signature cTitle">Escuchamos tus ideas, metas y objetivos.</small>
-                      <p>
-                        Exploramos juntos las necesidades digitales que tiene tu negocio.
-                      </p>
-                    </li>
+            <ol class="features">
+              <li>
+                <small class="signature cTitle">Escuchamos tus ideas, metas y objetivos.</small>
+                <p>
+                  Exploramos juntos las necesidades digitales que tiene tu negocio.
+                </p>
+              </li>
 
-                    <li>
-                      <small class="signature cTitle">Creamos una estrategia digital.</small>
-                      <p>
-                        En base a las necesidades de tu negocio, hacemos una estrategia para atraer clientes potenciales desde internet.
-                      </p>
-                    </li>
+              <li>
+                <small class="signature cTitle">Creamos una estrategia digital.</small>
+                <p>
+                  En base a las necesidades de tu negocio, hacemos una estrategia para atraer clientes potenciales desde internet.
+                </p>
+              </li>
 
-                    <li>
-                      <small class="signature cTitle">Diseñamos tu página web.</small>
-                    </li>
-                    <li>
-                      <small class="signature cTitle">Proponemos una campaña de redes sociales</small>
-                      <p>
-                        Incluye un diseño de una línea gráfica, concepto de estrategia y contenido de valor.
-                      </p>
-                    </li>
-                  </ol>
+              <li>
+                <small class="signature cTitle">Diseñamos tu página web.</small>
+              </li>
+              <li>
+                <small class="signature cTitle">Proponemos una campaña de redes sociales</small>
+                <p>
+                  Incluye un diseño de una línea gráfica, concepto de estrategia y contenido de valor.
+                </p>
+              </li>
+            </ol>
 
-                </div>
-              </div>
-            </div>
           </div>
         </div>
       </div>
     </div>
   </div>
+</div>
+</div>
+</div>
